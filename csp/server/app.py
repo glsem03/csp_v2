c4f233da592b77576360b79e91784f6a0b59be70
OtvalShpindela <72508994+OtvalShpindela@users.noreply.github.com>
2021-12-21 17:07:44 +0700

    add Users

--- csp/server/app.py
+++ csp/server/app.py
@@ -1,12 +1,27 @@
 from flask import Flask
 import os
 import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_login import  LoginManager
 
 DATABASE = '/'
 DEBUG = True
 SECRET_KEY = 'CAJhjasklGUgklA<4%^g,b.jgvnb'
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite3:///nkedb.db'
+
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50), unique=True)
+    mail = db.Column(db.String(50), unique=True)
+    psw = db.Column(db.String(500), nullable=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_type = db.Column(db.String(50), default='student')
 
 
 @app.route('/')
