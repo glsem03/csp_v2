6b8eb8ad82762a73dbca3161e271456438875a08
linkqw <q5481996@gmail.com>
2021-12-29 13:17:13 +0700

    Add html and sign up

--- csp/server/app.py
+++ csp/server/app.py
@@ -1,27 +1,44 @@
-from flask import Flask
-import os
-import sqlite3
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_login import LoginManager
 
-DATABASE = '/'
-DEBUG = True
-SECRET_KEY = 'CAJhjasklGUgklA<4%^g,b.jgvnb'
+SECRET_KEY = 'so-so-so-so-so-difficult-key'
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sql:///nkedb.db'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///datebase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 
-class Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    login = db.Column(db.String(50), unique=True)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    login = db.Column(db.String(50), unique=True, nullable=False)
     mail = db.Column(db.String(50), unique=True)
     psw = db.Column(db.String(500), nullable=True)
     date = db.Column(db.DateTime, default=datetime.utcnow)
-    user_type = db.Column(db.String(50), default='student')
+    user_type = db.Column(db.String(50), default='anonim')
+
+    def __repr__(self):
+        return '<Users %r' % self.id
+
+
+class LoginForm:
+    validate_on_submit = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(15), primary_key=True, nullable=False)
+    password = db.Column(db.String(500), nullable=True)
+    remember_me = db.Column(db.String(5), nullable=True)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
+
+
+@app.route('/s-u')
+def Sign_up():
+    return render_template('base.html')
 
 
 @app.route('/')
